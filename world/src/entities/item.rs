bc0a1de9b4007fb8c6da06a67af4829f82ac3f6e
DrThum <thum@drthum.net>
2023-04-02 21:30:34 +0200

    Temporarily disable sending Item updates to prevent a client crash at login

--- world/src/entities/item.rs
+++ world/src/entities/item.rs
@@ -65,7 +65,8 @@ impl UpdatableEntity for Item {
             blocks: vec![self.gen_create_data()],
         };
 
-        vec![update_data]
+        // vec![update_data]
+        Vec::new()
     }
 
     fn get_update_data(&self) -> Vec<UpdateData> {

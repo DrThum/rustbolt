a1e7aefb686270563ab9d6fa4edf72142370a5f5
DrThum <thum@drthum.net>
2024-03-04 22:03:54 +0100

    Stitch minimap tiles together to make full maps

---
+++ tools/minimap_extractor/src/lib.rs
@@ -0,0 +1,65 @@
+use regex::Regex;
+use tools_shared::mpq_manager::MPQManager;
+
+pub async fn get_trs_lines(manager: &MPQManager) -> Vec<u8> {
+    manager
+        .get_file_data("textures\\Minimap\\md5translate.trs".to_string())
+        .await
+        .await
+        .expect("unable to find md5translate.trs file")
+        .expect("unable to find md5translate.trs file")
+        .expect("unable to find md5translate.trs file") // lol
+}
+
+pub fn extract_tile_info_from_trs_line(line: &str) -> Option<TileInfo> {
+    let parts: Vec<&str> = line.split("\t").collect();
+    let hashed_file_name = parts[1]; // The actual file in the MPQ with the MD5 hash as a name
+    let tile_name = parts[0]; // The map tile it represents
+
+    let re = Regex::new("(.*)\\\\map([0-9]+)_([0-9]+)\\.blp").unwrap();
+    for (_, [map_name, tile_x, tile_y]) in re.captures_iter(tile_name).map(|c| c.extract()) {
+        return Some(TileInfo {
+            hashed_file_name,
+            map_name,
+            tile_x: tile_x.parse().unwrap(),
+            tile_y: tile_y.parse().unwrap(),
+        });
+    }
+
+    // panic!("unable to extract TileInfo from md5translate.trs line");
+    None
+}
+
+pub struct TileInfo<'a> {
+    pub hashed_file_name: &'a str,
+    pub map_name: &'a str,
+    pub tile_x: u32,
+    pub tile_y: u32,
+}
+
+// Represents the min and max X and Y found for a given map (in mapXX_YY.blp)
+pub struct Bounds {
+    pub start_x: u32, // First tile
+    pub start_y: u32,
+    pub end_x: u32, // Last tile
+    pub end_y: u32,
+}
+
+impl Bounds {
+    pub fn reset(&mut self) {
+        self.start_x = u32::MAX;
+        self.start_y = u32::MAX;
+
+        self.end_x = u32::MIN;
+        self.end_y = u32::MIN;
+    }
+
+    // Enlarge bounds if needed
+    pub fn refresh(&mut self, candidate_x: u32, candidate_y: u32) {
+        self.start_x = candidate_x.min(self.start_x);
+        self.start_y = candidate_y.min(self.start_y);
+
+        self.end_x = candidate_x.max(self.end_x);
+        self.end_y = candidate_y.max(self.end_y);
+    }
+}
--- tools/minimap_extractor/src/main.rs
+++ tools/minimap_extractor/src/main.rs
@@ -1,4 +1,6 @@
+use image::{DynamicImage, GenericImage};
 use image_blp::{convert::blp_to_image, parser::load_blp_from_buf};
+use minimap_extractor::{extract_tile_info_from_trs_line, get_trs_lines, Bounds};
 use tools_shared::mpq_manager::MPQManager;
 
 use std::{io::BufRead, path::PathBuf};
@@ -13,16 +15,19 @@ async fn main() -> Result<(), std::io::Error> {
     let client_data_dir = args.client_base_dir.to_str().unwrap();
     let manager = MPQManager::new(client_data_dir)?;
 
-    let trs = manager
-        .get_file_data("textures\\Minimap\\md5translate.trs".to_string())
-        .await
-        .await
-        .expect("unable to find md5translate.trs file")
-        .expect("unable to find md5translate.trs file")
-        .expect("unable to find md5translate.trs file"); // lol
-
+    let trs = get_trs_lines(&manager).await;
     let mut cursor = std::io::Cursor::new(trs);
     let mut buffer = vec![];
+
+    let mut bounds = Bounds {
+        start_x: 0,
+        start_y: 0,
+        end_x: 0,
+        end_y: 0,
+    };
+    bounds.reset();
+
+    let mut tiles: Vec<(String, u32, u32, DynamicImage)> = vec![];
 
     while let Ok(_) = cursor.read_until(0x0A as u8, &mut buffer) {
         if let Ok(line) = std::str::from_utf8(&buffer) {
@@ -30,36 +35,82 @@ async fn main() -> Result<(), std::io::Error> {
                 break;
             }
 
-            // We don't need these lines, the full path is included for each tile
-            if line.starts_with("dir:") {
+            // TEMP REMOVEME
+            if line.starts_with("WMO") {
                 buffer.clear();
+                bounds.reset();
+                tiles.clear();
+
                 continue;
             }
 
-            let parts: Vec<&str> = line.split("\t").collect();
-            let tile_file = parts[1]; // The actual file in the MPQ with the MD5 hash as a name
-            let tile_name = parts[0]; // The map tile it represents
+            // We start the processing of a new map
+            if line.starts_with("dir:") {
+                if !tiles.is_empty() {
+                    println!("\tStitching map ({} tiles)...", tiles.len());
+                    // TODO: for non-WMO maps each tile is 256x256, but for WMOs we're gonna need
+                    // data from the MOGI header
 
-            let mut full_path = format!("textures\\Minimap\\{}", tile_file);
-            // Remove the trailing \r\n
-            full_path.truncate(full_path.len() - 2);
+                    let stitched_width_px = (bounds.end_x - bounds.start_x + 1) * 256;
+                    let stitched_height_px = (bounds.end_y - bounds.start_y + 1) * 256;
 
-            let blp_data = manager
-                .get_file_data(full_path)
-                .await
-                .await
-                .unwrap()
-                .unwrap()
-                .unwrap();
-            let blp_image = load_blp_from_buf(&blp_data).unwrap();
-            let image = blp_to_image(&blp_image, 0).expect("BlpImage to DynamicImage failed");
-            image
-                .save(format!(
-                    "{}/{}",
-                    args.output_dir.to_str().unwrap(),
-                    tile_name.replace("\\", "_").replace(".blp", ".png")
-                ))
-                .unwrap();
+                    let mut stitched =
+                        DynamicImage::new_rgba16(stitched_width_px, stitched_height_px);
+
+                    for (_, x, y, tile) in &tiles {
+                        stitched
+                            .copy_from(
+                                tile,
+                                (*x - bounds.start_x) * 256,
+                                (*y - bounds.start_y) * 256,
+                            )
+                            .unwrap();
+                    }
+
+                    stitched
+                        .save(format!("out/{}_full.png", &tiles.first().unwrap().0))
+                        .unwrap();
+                }
+
+                print!("Processing Map {}", line.replace("dir: ", ""));
+                buffer.clear();
+                bounds.reset();
+                tiles.clear();
+
+                continue;
+            }
+
+            if let Some(tile_info) = extract_tile_info_from_trs_line(line) {
+                let mut full_path = format!("textures\\Minimap\\{}", tile_info.hashed_file_name);
+                // Remove the trailing \r\n
+                full_path.truncate(full_path.len() - 2);
+
+                bounds.refresh(tile_info.tile_x, tile_info.tile_y);
+
+                let blp_data = manager
+                    .get_file_data(full_path)
+                    .await
+                    .await
+                    .unwrap()
+                    .unwrap()
+                    .unwrap();
+                let blp_image = load_blp_from_buf(&blp_data).unwrap();
+                let image = blp_to_image(&blp_image, 0).expect("BlpImage to DynamicImage failed");
+
+                tiles.push((
+                    tile_info.map_name.to_string(),
+                    tile_info.tile_x,
+                    tile_info.tile_y,
+                    image,
+                ));
+            }
+            // image
+            //     .save(format!(
+            //         "{}/{}",
+            //         args.output_dir.to_str().unwrap(),
+            //         tile_name.replace("\\", "_").replace(".blp", ".png")
+            //     ))
+            //     .unwrap();
         }
 
         buffer.clear();

dd75d6db7eb2ce18b34e1de9a1ee728bec14ddce
DrThum <thum@drthum.net>
2025-03-26 15:36:30 +0100

    fix(packets): don't borrow AllStoragesViewMut unnecessarily in sys/process_packets

--- world/src/ecs/systems/packets.rs
+++ world/src/ecs/systems/packets.rs
@@ -1,27 +1,25 @@
-use shipyard::{AllStoragesViewMut, UniqueView};
+use shipyard::UniqueView;
 
 use crate::{
     game::{packet_queue::WrappedPacketQueue, world_context::WrappedWorldContext},
     session::opcode_handler::PacketHandlerArgs,
 };
 
-pub fn process_packets(vm_all_storages: AllStoragesViewMut) {
-    vm_all_storages.run(
-        |packet_queue: UniqueView<WrappedPacketQueue>,
-         world_context: UniqueView<WrappedWorldContext>| {
-            let world_context = world_context.0.clone();
+pub fn process_packets(
+    packet_queue: UniqueView<WrappedPacketQueue>,
+    world_context: UniqueView<WrappedWorldContext>,
+) {
+    let world_context = world_context.0.clone();
 
-            for (session, packet) in packet_queue.get_packets_and_reset_queue() {
-                let (_, handler) = world_context
-                    .opcode_handler
-                    .get_handler(packet.header.opcode);
+    for (session, packet) in packet_queue.get_packets_and_reset_queue() {
+        let (_, handler) = world_context
+            .opcode_handler
+            .get_handler(packet.header.opcode);
 
-                handler(PacketHandlerArgs {
-                    session: session.clone(),
-                    world_context: world_context.clone(),
-                    data: packet.payload,
-                });
-            }
-        },
-    );
+        handler(PacketHandlerArgs {
+            session: session.clone(),
+            world_context: world_context.clone(),
+            data: packet.payload,
+        });
+    }
 }

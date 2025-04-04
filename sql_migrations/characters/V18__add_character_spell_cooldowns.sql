6d27244ebefcda60606b8d8ccd2a15b8e15b3dc3
DrThum <thum@drthum.net>
2025-04-04 22:00:28 +0200

    fix(sql): add a foreign key on character_spell_cooldowns.character_guid (yes I modified the migration, who cares I'm the only one here)

--- sql_migrations/characters/V18__add_character_spell_cooldowns.sql
+++ sql_migrations/characters/V18__add_character_spell_cooldowns.sql
@@ -2,7 +2,8 @@ CREATE TABLE character_spell_cooldowns(
   character_guid INTEGER NOT NULL,
   spell_id INTEGER NOT NULL,
   item_id INTEGER,
-  cooldown_end_timestamp INTEGER NOT NULL
+  cooldown_end_timestamp INTEGER NOT NULL,
+  FOREIGN KEY(character_guid) REFERENCES characters(guid) ON DELETE CASCADE
 );
 
 CREATE UNIQUE INDEX idx_character_spell_cooldowns_spell_id ON character_spell_cooldowns(character_guid, spell_id);
